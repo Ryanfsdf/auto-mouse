ef053d2b78665e609ad8be93fee5dd79771a4b99
Ryanfsdf <ryanfsdf@live.ca>
2016-03-13 05:32:27 -0400

    Adjusted Speed

--- src/sample/TheRobot.java
+++ src/sample/TheRobot.java
@@ -8,29 +8,29 @@ public class TheRobot {
             Robot robot = new Robot();
             for (int x = 0; x < MouseListener.savedMouseState.size() - 1; x++) {
                 if (MouseListener.savedMouseState.get(x).getState() == 0) {
-                    robot.delay(5);
+                    robot.delay(25);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 1) {
-                    robot.delay(75);
+                    robot.delay(275);
                     robot.mousePress(16);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 2) {
-                    robot.delay(75);
+                    robot.delay(275);
                     robot.mouseRelease(16);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 3) {
-                    robot.delay(75);
+                    robot.delay(275);
                     robot.mousePress(16);
                     robot.mouseRelease(16);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 4) {
-                    robot.delay(75);
+                    robot.delay(275);
                     robot.keyPress(MouseListener.savedMouseState.get(x).getXCoordinate());
-                    robot.delay(75);
+                    robot.delay(275);
                     robot.keyRelease(MouseListener.savedMouseState.get(x).getXCoordinate());
                 }
             }
