8f52dff67b6fdbffb5f26a84a6628fbd8bd5c847
Ryanfsdf <ryanfsdf@live.ca>
2016-03-13 05:39:25 -0400

    Switched from robot.delay to Thread.sleep for OS X compatibility

--- src/sample/TheRobot.java
+++ src/sample/TheRobot.java
@@ -8,29 +8,54 @@ public class TheRobot {
             Robot robot = new Robot();
             for (int x = 0; x < MouseListener.savedMouseState.size() - 1; x++) {
                 if (MouseListener.savedMouseState.get(x).getState() == 0) {
-                    robot.delay(25);
+                    try {
+                        Thread.sleep(5);
+                    }
+                    catch (InterruptedException ie) {
+                    }
+
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 1) {
-                    robot.delay(275);
+                    try {
+                        Thread.sleep(275);
+                    }
+                    catch (InterruptedException ie) {
+                    }
                     robot.mousePress(16);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 2) {
-                    robot.delay(275);
+                    try {
+                        Thread.sleep(275);
+                    }
+                    catch (InterruptedException ie) {
+                    }
                     robot.mouseRelease(16);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 3) {
-                    robot.delay(275);
+                    try {
+                        Thread.sleep(275);
+                    }
+                    catch (InterruptedException ie) {
+                    }
                     robot.mousePress(16);
                     robot.mouseRelease(16);
                     robot.mouseMove(MouseListener.savedMouseState.get(x).getXCoordinate(), MouseListener.savedMouseState.get(x).getYCoordinate());
                 }
                 else if (MouseListener.savedMouseState.get(x).getState() == 4) {
-                    robot.delay(275);
+                    try {
+                        Thread.sleep(275);
+                    }
+                    catch (InterruptedException ie) {
+                    }
                     robot.keyPress(MouseListener.savedMouseState.get(x).getXCoordinate());
-                    robot.delay(275);
+                    try {
+                        Thread.sleep(275);
+                    }
+                    catch (InterruptedException ie) {
+                    }
                     robot.keyRelease(MouseListener.savedMouseState.get(x).getXCoordinate());
                 }
             }
